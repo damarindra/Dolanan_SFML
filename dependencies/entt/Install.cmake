c70436777e6cee97adaccb0186a2d63aea11d363
Damar Inderajati <damarind@gmail.com>
2020-06-25 20:22:57 +0700

    add cute_c2 as a simple collision solution

---
+++ dependencies/entt/Install.cmake
@@ -0,0 +1,19 @@
+include(GNUInstallDirs)
+
+install(TARGETS Dolanan
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+if(NOT LINK_DEPS_STATIC)
+    set_target_properties(Dolanan PROPERTIES INSTALL_RPATH $ORIGIN/../${CMAKE_INSTALL_LIBDIR})
+
+    set_target_properties(entt PROPERTIES INSTALL_RPATH $ORIGIN)
+
+    install(TARGETS
+            entt
+            PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            NAMELINK_SKIP)
+endif()
+
+include(CPack)
